c450cf726754d9b5ee2fe56133c3799767bed1dc
DaehyeongLee <apm5018@naver.com>
2024-03-07 23:51:50 +0900

    현재 위치를 가져오는 로직 추가하고 화면에 표시

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("");
+  const [isOk, setIsOk] = useState(true);
+
+  const requestLocation = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setIsOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    requestLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{isOk ? city : "Loading..."}</Text>
       </View>
       <ScrollView
         horizontal
